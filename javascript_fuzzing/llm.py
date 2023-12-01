a5a0c7166f1a8ae1fbbc500d179d55018331a947
Daniel Chong <danielchong@Daniels-MacBook-Pro-3.local>
2023-12-01 15:27:18 -0500

    update

--- javascript_fuzzing/llm.py
+++ javascript_fuzzing/llm.py
@@ -27,29 +27,9 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
 #     print(str(response.choices[0].message))
     return response.choices[0].message["content"]
 
-def get_base_prompt(base="java"):
-    if base == "python":
-        file_name = "py_base_prompt.txt"
-    elif base == "test":
-        file_name = "test_base_prompt.txt"
-    elif base == "java":
-        file_name = "java_base_prompt.txt"
-    else:
-        file_name = "c_base_prompt.txt"
-    with open(file_name, 'r') as f:
-        base_prompt = f.read()
-    return base_prompt
-
-def get_mutate_prompt(base="python"):
-    if base == "python":
-        file_name = "py_mutation_prompt.txt"
-    with open(file_name, 'r') as f:
-        mutate_prompt = f.read()
-    return mutate_prompt
-
 def add_context(context, role, content):
     context.append({'role': role, 'content': content})
-    response = get_completion_from_messages(context, temperature=0)
+    response = get_completion_from_messages(context, temperature=0.25)
     context.append({'role': 'assistant', 'content': response})
     print(response)
 
@@ -63,6 +43,10 @@ def test(context):
     print("last context:")
     print(context[-1]['content'])
     quit()
+
+def reset_context(context):
+    context = [{'role': 'system', 'content': "You are a coding tool and \
+                reply ONLY with JAVASCRIPT CODE."}]
 
 def write_output(file_name,output):
     with open(file_name, "w") as f:
@@ -80,25 +64,19 @@ def main():
                 reply ONLY with JAVASCRIPT CODE."}]
 
     input_files = ["snippets/snippet.js"]
-    loops,conditionals = parser.parse_structures(input_files[0])
     output_file = "output.js"
+    loops,conditionals = parser.parse_structures(input_files[0])
+
     #snippet = formatter.label_lines(input_files[0])
     with open(input_files[0], "r") as f:
         snippet = f.read()
-    #combine([output_file, "snippets/snippet3"], context, output_file)
-    add_context(context, 'user', prompter.inject_statement(snippet, context))
+    #prompt, code = prompter.inject_statement(snippet, context, 4, 2)
+    #add_context(context, 'user', prompt)
+    #write_output(output_file, code.replace(prompter.delimiter,context[-1]['content']))
+    #reset_context(context)
+    prompt = prompter.extend(snippet, context)
+    add_context(context, 'user', prompt)
     write_output(output_file, context[-1]['content'])
-
-    #complicate(output_file, context, output_file)
-    #extend(output_file, context, output_file)
-    #extend("snippet2",context)
-    #base_prompt = get_base_prompt("java")
-    #add_context(context, 'user', base_prompt)
-
-    #with open("llm_out.txt", "w") as f:
-    #    f.write(context[-1]['content'])
-    #with open("mutation_out.txt", "w") as f:
-    #    f.write(context[-1]['content'])
 
 if __name__ == "__main__":
     main()
--- javascript_fuzzing/output.js
+++ javascript_fuzzing/output.js
@@ -1,19 +1,31 @@
-// Check for special characters
-if (!letter.match(/[!@#%&*]/)) {
-  yield letter;
+function* invalidControls() {
+  // Check upper case Cyrillic
+  for (var alpha = 0x0410; alpha <= 0x042F; alpha++) {
+    yield String.fromCharCode(alpha);
+  }
+
+  // Check lower case Cyrillic
+  for (alpha = 0x0430; alpha <= 0x044F; alpha++) {
+    yield String.fromCharCode(alpha);
+  }
+
+  // Check ASCII characters which are not in the extended range or syntax
+  // characters
+  for (alpha = 0x00; alpha <= 0x7F; alpha++) {
+    let letter = String.fromCharCode(alpha);
+    if (!letter.match(/[0-9A-Za-z_\$(|)\[\]\/\\^]/)) {
+      yield letter;
+    }
+  }
+
+  // Check for end of string
+  yield "";
 }
 
-// Check for punctuation marks
-if (!letter.match(/[.,:;?!]/)) {
-  yield letter;
-}
+const invalidControlGenerator = invalidControls();
 
-// Check for mathematical symbols
-if (!letter.match(/[+\-*/=<>]/)) {
-  yield letter;
-}
-
-// Check for whitespace characters
-if (!letter.match(/\s/)) {
-  yield letter;
-}
+// Example usage:
+console.log(invalidControlGenerator.next().value); // Output: "А"
+console.log(invalidControlGenerator.next().value); // Output: "Б"
+console.log(invalidControlGenerator.next().value); // Output: "В"
+// ... continue calling invalidControlGenerator.next().value to get the next invalid control character
