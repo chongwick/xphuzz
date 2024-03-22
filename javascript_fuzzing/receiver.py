2aa94cd6f6b1a9156a78fde0031378d7fda7fed9
d-chong <56747135+d-chong@users.noreply.github.com>
2024-03-22 15:07:48 -0400

    update

--- javascript_fuzzing/receiver.py
+++ javascript_fuzzing/receiver.py
@@ -1,18 +1,20 @@
 import pickle
 import os
 
-output_file = "__output__"
+llm_workdir = "llm_workdir/"
+output_file = llm_workdir + "__output__"
 is_output = lambda : os.path.isfile(output_file)
-terminate_file = "__terminate__"
-llm_type_file = "__llm_type__"
-llm_query_file = "__llm_query__"
+terminate_file = llm_workdir + "__terminate__"
+llm_type_file = llm_workdir + "__llm_type__"
+llm_query_file = llm_workdir + "__llm_query__"
+arguments_file = llm_workdir + "arguments.pickle"
 
 def send_command_file(file, content=""):
     with open(file, "w") as f:
         f.write(content)
 
 def submit(arguments):
-    with open("arguments.pickle", "wb") as f:
+    with open(arguments_file, "wb") as f:
         pickle.dump(arguments,f)
     send_command_file(llm_query_file)
     while(not(is_output())):
@@ -26,7 +28,7 @@ class Chat_LLM:
     def __init__(self, context, temperature=0.1):
         self.context = context
         arguments = {"context" : context}
-        with open("arguments.pickle", "wb") as f:
+        with open(arguments_file, "wb") as f:
             pickle.dump(arguments,f)
         send_command_file(llm_type_file, "chat")
         while(not(is_output())):
@@ -49,11 +51,65 @@ class Chat_LLM:
         arguments = {
                 'command':"add_context",'params':[role, content]
                 }
-        return submit(arguments)
+        result = submit(arguments)
+        #idk maybe return this who knows
+        self.context.append({'role': 'assistant', 'content': result})
+        return self.context
+        #return result
 
     def reset_context(self):
+        self.context = self.original_context
         arguments = {
                 'command':"reset_context",'params':[]
                 }
         return submit(arguments)
 
+class Completion_LLM:
+    def __init__(self, temperature=0.1):
+        send_command_file(llm_type_file, "completion")
+        while(not(is_output())):
+            pass
+        os.remove(output_file)
+
+    def change_temperature(self, temperature):
+        arguments = {
+                'command':"change_temperature",'params':[temperature]
+                }
+        return submit(arguments)
+
+    def change_response_max_length(self, length):
+        arguments = {
+                'command':"change_response_max_length",'params':[length]
+                }
+        return submit(arguments)
+
+    def get_completion(self, content):
+        arguments = {
+                'command':"get_completion",'params':[content]
+                }
+        return submit(arguments)
+
+class FIM_LLM:
+    def __init__(self, temperature=0.1):
+        send_command_file(llm_type_file, "fim")
+        while(not(is_output())):
+            pass
+        os.remove(output_file)
+
+    def change_temperature(self, temperature):
+        arguments = {
+                'command':"change_temperature",'params':[temperature]
+                }
+        return submit(arguments)
+
+    def change_response_max_length(self, length):
+        arguments = {
+                'command':"change_response_max_length",'params':[length]
+                }
+        return submit(arguments)
+
+    def fill_in_middle(self, content):
+        arguments = {
+                'command':"get_completion",'params':[content]
+                }
+        return submit(arguments)
--- javascript_fuzzing/star_llama.py
+++ javascript_fuzzing/star_llama.py
@@ -4,18 +4,85 @@ import torch
 import os
 import tiktoken
 import config as cfg
+import sys
+import pickle
 
 FIM_PREFIX = "<fim_prefix>"
 FIM_MIDDLE = "<fim_middle>"
 FIM_SUFFIX = "<fim_suffix>"
 EOF = "<file_sep>"
-
 FIM_INDICATOR = "<FILL_HERE>"
+
+CHAT = "chat"
+COMPLETION = "completion"
+FIM = "fim"
+
+llm_workdir = "llm_workdir/"
+terminate_file = llm_workdir + "__terminate__"
+llm_type_file = llm_workdir + "__llm_type__"
+#Arguments file shall have the format:
+# {'command': function, 'params':[function parameters]}
+llm_query_file = llm_workdir + "__llm_query__"
+arguments_file = llm_workdir + "arguments.pickle"
+output_file = llm_workdir + "__output__"
+
+#class llm():
+#    def __init__(self, context):
+#        self.hi = 1
+#    def change_response_max_length(self,x):
+#        print("here")
+#        self.hi = 12
+
+def get_arguments(arguments_file):
+    with open(arguments_file, "rb") as f:
+        arguments = pickle.load(f)
+    return arguments
+
+def execute_function(llm_type, llm_object, arguments):
+    llm_functions = None
+    if llm_type == CHAT:
+        llm_functions = CHAT_FUNCTIONS
+    elif llm_type == COMPLETION:
+        llm_functions = COMPLETION_FUNCTIONS
+    elif llm_type == FIM_FUNCTIONS:
+        llm_functions = FIM_FUNTIONS
+    function = llm_functions[arguments['command']]
+    params = [llm_object] + arguments['params'] #Prepend self to the arguments
+    return(function(*params))
 
 def num_tokens_from_string(string, encoding_name="cl100k_base"):
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
     return num_tokens
+
+CHAT_FUNCTIONS = {
+        "change_response_max_length":
+        lambda llm, l : llm.change_response_max_length(l),
+        "change_temperature":
+        lambda llm, t : llm.change_temperature(t),
+        "add_context":
+        lambda llm, r, c: llm.add_context(r,c),
+        "reset_context":
+        lambda llm : llm.reset_context(),
+        "change_role":
+        lambda llm, rd : llm.change_role(rd),
+}
+COMPLETION_FUNCTIONS = {
+        "change_response_max_length":
+        lambda llm, l : llm.change_response_max_length(l),
+        "change_temperature":
+        lambda llm, t : llm.change_temperature(t),
+        "get_completion":
+        lambda llm, c : llm.get_completion(c),
+}
+FIM_FUNCTIONS = {
+        "change_response_max_length":
+        lambda llm, l : llm.change_response_max_length(l),
+        "change_temperature":
+        lambda llm, t : llm.change_temperature(t),
+        "fill_in_middle":
+        lambda llm, c : llm.fill_in_middle(c),
+}
 
 class Chat_LLM:
     def __init__(self, context, temperature=0.1):
@@ -59,6 +126,7 @@ class Chat_LLM:
         decoded = self.tokenizer.decode(output)
         response = decoded.split("[/INST]")[-1].split("</s>")[0].lstrip()
         self.context.append({'role': 'assistant', 'content': response})
+        return response
         #print(response)
 
     def reset_context(self):
@@ -144,3 +212,54 @@ class FIM_LLM:
         fim_content = generation[0].split(FIM_MIDDLE)[1].split(EOF)[0]
         content.replace(FIM_INDICATOR, fim_content)
         return content
+
+def main():
+    llm_type = None
+    llm_object = None
+
+    is_terminate = lambda : os.path.isfile(terminate_file)
+    set_llm_type = lambda : os.path.isfile(llm_type_file)
+    is_query = lambda : os.path.isfile(llm_query_file)
+
+    while(True):
+
+        #Check for termination/change command files
+        if is_terminate():
+            os.remove(terminate_file)
+            quit()
+        elif set_llm_type():
+            with open(llm_type_file, "r") as f:
+                llm_type = f.read()
+                del(llm_object)
+                torch.cuda.empty_cache()
+                if "chat" in llm_type:
+                    llm_type = CHAT
+                    arguments = get_arguments(arguments_file)
+                    context = arguments['context']
+                    llm_object = Chat_LLM(context)
+                elif "completion" in llm_type:
+                    llm_type = COMPLETION
+                    llm_object = Completion_LLM()
+                elif "fim" in llm_type:
+                    llm_type = FIM
+                    llm_object = FIM_LLM()
+                with open(output_file, "w") as f:
+                    f.write("1")
+                os.remove(llm_type_file)
+                if os.path.isfile(arguments_file):
+                    os.remove(arguments_file)
+        elif is_query():
+            arguments = get_arguments(arguments_file)
+            os.remove(llm_query_file)
+            os.remove(arguments_file)
+            result = execute_function(llm_type, llm_object, arguments)
+            with open(output_file, "w") as f:
+                if result != None:
+                    f.write(result)
+                else:
+                    f.write("1")
+        else:
+            pass
+
+if __name__ == "__main__":
+    main()
