899fe809b3d2729c6243a923b74ffc3e5c5f41b1
d-chong <56747135+d-chong@users.noreply.github.com>
2024-05-17 13:54:40 -0400

    update

---
+++ php_fuzzing/clear_queues.py
@@ -0,0 +1,18 @@
+import config as cfg
+import pickle
+
+with open(cfg.seed_data, "wb") as f:
+    g = {}
+    pickle.dump(g,f,pickle.HIGHEST_PROTOCOL)
+
+with open(cfg.llm_queue, "wb") as f:
+    g = []
+    pickle.dump(g,f,pickle.HIGHEST_PROTOCOL)
+
+with open(cfg.cov_queue, "wb") as f:
+    g = []
+    pickle.dump(g,f,pickle.HIGHEST_PROTOCOL)
+
+with open(cfg.san_queue, "wb") as f:
+    g = []
+    pickle.dump(g,f,pickle.HIGHEST_PROTOCOL)
--- php_fuzzing/config.py
+++ php_fuzzing/config.py
@@ -4,9 +4,10 @@ sanitizer_engine = "/home/w023dtc/php_engines/san_php"
 release_engine = "/home/w023dtc/php_engines/rel_php"
 model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 llm_requests = "llm_requests"
-llm_queue = "llm_requests/queue.pickle"
 seed_data = "seed_data.pickle"
 php_corpus = "php_corpus"
+
+llm_queue = "llm_requests/queue.pickle"
 cov_queue = "cov_queue.pickle"
 san_queue = "san_queue.pickle"
 
