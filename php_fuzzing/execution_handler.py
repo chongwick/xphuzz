1a59415651a55325a93ccf8306e2642b504ceda5
d-chong <56747135+d-chong@users.noreply.github.com>
2025-04-25 14:16:48 -0400

    update

--- php_fuzzing/execution_handler.py
+++ php_fuzzing/execution_handler.py
@@ -53,49 +53,48 @@ def exec_loop():
             is_error = lambda x: os.path.exists(x+".er")
             is_trash = lambda x: os.path.exists(x+".tr")
 
-            for i in range(2):
-                command = ['bash','./sanitize.sh',os.path.join(os.getcwd(),php_file),str(i),cfg.sanitizer_engine]
+            command = ['bash','./sanitize.sh',os.path.join(os.getcwd(),php_file)]
 
-                child = None
-                try:
-                    child = subprocess.run(command,
-                                           text=True,
-                                           timeout=40,
-                                           capture_output=True)
-                except subprocess.TimeoutExpired as exc:
-                    valid = False
-                    break
-                if is_error(php_file):
-                    php_file = php_file+".er"
-                    crash = None
-                    error = None
-                    #category = None
-                    if "exit(" in code:
-                        crash = "NC"
-                        error = 'exit'
-                    else:
-                        crash = i
-                        error = utils.read_file(cfg.san_log)
-                        #category = None
-                        try:
-                            error = error.split("/w023dtc/")[1].split(" ")[0] #change this
-                        except Exception as e:
-                            error = error
-                    bugs = utils.load_pickle(cfg.bug_log)
-                    if error not in bugs:
-                        bugs[error] = [php_file]
-                    else:
-                        bugs[error].append(php_file)
-                    utils.dump_pickle(cfg.bug_log,bugs)
-                    break
+            child = None
+            try:
+                child = subprocess.run(command,
+                                       text=True,
+                                       timeout=40,
+                                       capture_output=True)
+            except subprocess.TimeoutExpired as exc:
+                valid = False
+                break
+            if is_error(php_file):
+                php_file = php_file+".er"
+                crash = None
+                error = None
+                #category = None
+                if "exit(" in code:
+                    crash = "NC"
+                    error = 'exit'
                 else:
-                    if is_trash(php_file):
-                        os.remove(php_file+".tr")
-                    else:
-                        try:
-                            os.remove(php_file)
-                        except Exception as e: #I'll handle this eventually
-                            pass
+                    crash = i
+                    error = utils.read_file(cfg.san_log)
+                    #category = None
+                    try:
+                        error = error.split("/w023dtc/")[1].split(" ")[0] #change this
+                    except Exception as e:
+                        error = error
+                bugs = utils.load_pickle(cfg.bug_log)
+                if error not in bugs:
+                    bugs[error] = [php_file]
+                else:
+                    bugs[error].append(php_file)
+                utils.dump_pickle(cfg.bug_log,bugs)
+                break
+            else:
+                if is_trash(php_file):
+                    os.remove(php_file+".tr")
+                else:
+                    try:
+                        os.remove(php_file)
+                    except Exception as e: #I'll handle this eventually
+                        pass
 
         room_service(safe_files)
 
--- php_fuzzing/sanitize.sh
+++ php_fuzzing/sanitize.sh
@@ -2,22 +2,20 @@
 #Usage, ./sanitize.sh [target_script] [san_types(0/1)]
 
 script=$1
-san_type=$2 #0 for other, 1 for leak
+#san_type=$2 #0 for other, 1 for leak
 #php_engine=$3
-options=$4
-if [ $san_type = "0" ]; then
-        export USE_ZEND_ALLOC=0
-        export ASAN_OPTIONS=detect_leaks=0
-fi
+#options=$4
+
 #OUTPUT=$(timeout -s SIGTERM 120 php run-tests.php -p "$php_engine" "$script" 2>&1)
 #OUTPUT=$(timeout -s SIGTERM 120 ./run-tests.php -p "$php_engine" "$script" 2>&1)
-if [[ -z "$options" ]]; then
-        #OUTPUT=$(timeout -s SIGTERM 120 "$php_engine" "$script" 2>&1)
-        OUTPUT=$(timeout -s SIGTERM 120 ./nightly_php/php-src/sapi/cli/php "$script" 2>&1)
-else
-        #OUTPUT=$(timeout -s SIGTERM 120 "$php_engine" "$options" "$script" 2>&1)
-        OUTPUT=$(timeout -s SIGTERM 120 ./nightly_php/php-src/sapi/cli/php "$options" "$script" 2>&1)
-fi
+#if [[ -z "$options" ]]; then
+#        #OUTPUT=$(timeout -s SIGTERM 120 "$php_engine" "$script" 2>&1)
+#        OUTPUT=$(timeout -s SIGTERM 120 ./nightly_php/php-src/sapi/cli/php "$script" 2>&1)
+#else
+#        #OUTPUT=$(timeout -s SIGTERM 120 "$php_engine" "$options" "$script" 2>&1)
+#        OUTPUT=$(timeout -s SIGTERM 120 ./nightly_php/php-src/sapi/cli/php "$options" "$script" 2>&1)
+#fi
+OUTPUT=$(timeout -s SIGTERM 120 ./d8 --allow-natives-syntax "$script" 2>&1)
 RET=$?
 if [ $RET -ne 0 ]; then
        if [ $RET -eq 255 ]; then
@@ -36,15 +34,7 @@ if [ $RET -ne 0 ]; then
                mv "$script" "${script}.tr"
                exit 0
        fi
-       if [ $(echo "$OUTPUT" | grep "Allowed memory size of" | wc -l) -gt 0 ]; then
-               mv "$script" "${script}.tr"
-	       exit 0
-       fi
        if [ $(echo "$OUTPUT" | grep "AddressSanitizer failed to allocate" | wc -l) -gt 0 ]; then
-               mv "$script" "${script}.tr"
-	       exit 0
-       fi
-       if [ $(echo "$OUTPUT" | grep ": Assertion " | wc -l) -gt 0 ]; then
                mv "$script" "${script}.tr"
 	       exit 0
        fi
